58a18b10527acb10760cad4d2332e61326804c38
Honma Ryota <kabuprivate@gmail.com>
2022-02-14 23:56:50 +0900

    URLパラメーター

--- react-router/src/router/Router.jsx
+++ react-router/src/router/Router.jsx
@@ -1,7 +1,7 @@
 import {Route, Switch} from "react-router-dom";
 import {Home} from "../Home";
-import {Page2} from "../Page2";
 import {Page1Routes} from "./Page1Routes";
+import {Page2Routes} from "./Page2Routes";
 
 export const Router = () => {
   return (
@@ -10,21 +10,32 @@ export const Router = () => {
       <Route exact path="/">
         <Home />
       </Route>
+
       <Route
         path="/page1"
         render={({match: {url}}) => (
           <Switch>
-            {Page1Routes.map((route) => {
+            {Page1Routes.map((route) => (
               <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
                 {route.children}
-              </Route>;
-            })}
+              </Route>
+            ))}
           </Switch>
         )}
       />
-      <Route path="/page2">
-        <Page2 />
-      </Route>
+
+      <Route
+        path="/page2"
+        render={({match: {url}}) => (
+          <Switch>
+            {Page2Routes.map((route) => (
+              <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      />
     </Switch>
   );
 };
