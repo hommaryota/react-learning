15b301a4f0a52183c7ef4f56178cea7fe21763dc
hommaryota <kabuprivate@gmail.com>
2022-02-22 22:54:36 +0900

    test

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,18 +1,30 @@
+import axios from 'axios';
+import {useState  } from "react";
 import './App.css';
-import { Practice1 } from './practices/Practice1';
-import { Practice2 } from './practices/Practice2';
-import { Practice3 } from './practices/Practice3';
-import { Practice4 } from './practices/Practice4';
+import { Todo } from './Todo';
 
-function App() {
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export default function App() {
+  const [todos,setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
-    <div>
-      <Practice1 />
-      <Practice2/>
-      <Practice3/>
-      <Practice4/>
+    <div className='App'>
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo title={todo.title} userId={todo.userId} />
+      ))}
     </div>
   );
 }
-
-export default App;
