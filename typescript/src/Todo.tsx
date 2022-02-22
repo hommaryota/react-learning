4d70e09d2230f8705ae36eee4b02d04e4be7bfa1
hommaryota <kabuprivate@gmail.com>
2022-02-23 08:12:51 +0900

    PickとOmitで使用する型の指定を行う

--- typescript/src/Todo.tsx
+++ typescript/src/Todo.tsx
@@ -1,10 +1,10 @@
-type TodoType = {
-  userId: number;
-  title: string;
-  completed?: boolean;
-};
+// TodoTypeを読み込んでも、このファイルで全ての要素を使うわけではない
+// 使わない型が使用されていないエラーが出てしまうので、TodoTypeの中から何を使うかを指定する必要がある
+import { TodoType } from './types/todo';
 
-export const Todo = (props: TodoType) => {
+// Pickとは型の中から何を使うかを指定する
+// Omitとは型の中から何を使わないかを指定する
+export const Todo = (props: Omit<TodoType,"id">) => {
   const {title,userId,completed = false} = props;
   const completeMark = completed ? "[完]" : "[未]";
   return (
