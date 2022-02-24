0aa85e8b037882a2ecb1931d6dfc0f23d3560bd2
hommaryota <kabuprivate@gmail.com>
2022-02-24 19:21:51 +0900

    user一覧機能

--- chakra-ui/src/components/pages/UserManagement.tsx
+++ chakra-ui/src/components/pages/UserManagement.tsx
@@ -1,9 +1,13 @@
 import {memo, VFC} from "react";
+import {Box, Image, Stack, Text, Wrap, WrapItem} from "@chakra-ui/react";
+import {UserCard} from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <div>
-      <p>UserManagementページです</p>
-    </div>
+    <Wrap p={{base: 4, md: 10}}>
+      <WrapItem>
+        <UserCard imageUrl="https://source.unsplash.com/random" userName="かぶ" fullName="Ryota Hommma" />
+      </WrapItem>
+    </Wrap>
   );
 });
