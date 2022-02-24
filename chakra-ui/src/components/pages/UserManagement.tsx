9477071413dd4961e3fbb67476d2c082ee171c45
hommaryota <kabuprivate@gmail.com>
2022-02-25 08:07:59 +0900

    UserDetailModal created

--- chakra-ui/src/components/pages/UserManagement.tsx
+++ chakra-ui/src/components/pages/UserManagement.tsx
@@ -1,15 +1,18 @@
-import {memo, useEffect, VFC} from "react";
-import {Center, Spinner, Wrap, WrapItem} from "@chakra-ui/react";
+import {memo, useCallback, useEffect, VFC} from "react";
+import {Center, Spinner, useDisclosure, Wrap, WrapItem} from "@chakra-ui/react";
 
 import {UserCard} from "../organisms/user/UserCard";
 import {useAllUsers} from "../../hooks/useAllUsers";
+import {UserDetailModal} from "../organisms/user/UserDetailModal";
 
 export const UserManagement: VFC = memo(() => {
+  const {isOpen, onOpen, onClose} = useDisclosure();
+
   const {getUsers, loading, users} = useAllUsers();
 
-  useEffect(() => {
-    getUsers();
-  }, []);
+  useEffect(() => getUsers(), []);
+
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <>
@@ -21,11 +24,12 @@ export const UserManagement: VFC = memo(() => {
         <Wrap p={{base: 4, md: 10}} justify="space-around">
           {users.map((user) => (
             <WrapItem key={user.id} mx="auto">
-              <UserCard imageUrl="https://source.unsplash.com/random" userName={user.name} fullName={user.name} />
+              <UserCard imageUrl="https://source.unsplash.com/random" userName={user.name} fullName={user.name} onClick={onClickUser} />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
