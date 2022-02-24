7920ca68ded2a5ec87e518ae5921f73c6008262e
hommaryota <kabuprivate@gmail.com>
2022-02-24 17:36:03 +0900

    login機能追加

--- chakra-ui/src/components/pages/Login.tsx
+++ chakra-ui/src/components/pages/Login.tsx
@@ -1,7 +1,18 @@
-import {Box, Button, Divider, Flex, Heading, Input, Stack} from "@chakra-ui/react";
-import {memo, VFC} from "react";
+import {Box, Divider, Flex, Heading, Input, Stack} from "@chakra-ui/react";
+import {ChangeEvent, memo, useState, VFC} from "react";
+import {useAuth} from "../../hooks/useAuth";
+
+import {PrimaryButton} from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
+  const {login, loading} = useAuth();
+
+  const [userId, setUserId] = useState("");
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) => setUserId(e.target.value);
+
+  const onclickLogin = () => login(userId);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -10,10 +21,10 @@ export const Login: VFC = memo(() => {
         </Heading>
         <Stack spacing={6} py={4} px={10}>
           <Divider my={4} />
-          <Input placeholder="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{opacity: 0.8}}>
+          <Input placeholder="ユーザーID" value={userId} onChange={onChangeUserId} />
+          <PrimaryButton disabled={userId === ""} loading={loading} onClick={onclickLogin}>
             ログイン
-          </Button>
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
---
+++ chakra-ui/src/hooks/useAuth.ts
@@ -0,0 +1,34 @@
+import {useCallback, useState} from "react";
+import axios from "axios";
+import {useHistory} from "react-router-dom";
+
+import {User} from "../types/types/user";
+
+export const useAuth = () => {
+  const history = useHistory();
+
+  const [loading, setLoading] = useState(false);
+
+  const login = useCallback((id: string) => {
+    setLoading(true);
+
+    axios
+      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((res) => {
+        if (res.data) {
+          history.push("/home");
+        } else {
+          alert("ユーザーが見つかりません");
+        }
+      })
+      .catch(() => alert("ログインができません"))
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return {
+    login,
+    loading,
+  };
+};
