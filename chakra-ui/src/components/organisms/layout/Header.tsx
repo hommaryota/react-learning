24499e71fc82819f2c3ec68d3efab09e65123bb3
hommaryota <kabuprivate@gmail.com>
2022-02-24 12:09:25 +0900

    Header component分割

--- chakra-ui/src/components/organisms/layout/Header.tsx
+++ chakra-ui/src/components/organisms/layout/Header.tsx
@@ -1,6 +1,8 @@
-import {Box, Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Heading, IconButton, Link, useDisclosure} from "@chakra-ui/react";
+import {Box, Flex, Heading, Link, useDisclosure} from "@chakra-ui/react";
 import {memo, VFC} from "react";
-import {HamburgerIcon} from "@chakra-ui/icons";
+
+import {MenuIconButton} from "../../atoms/button/MenuIconButton";
+import {MenuDrawer} from "../../molecules/MenuDrawer";
 
 export const Header: VFC = memo(() => {
   const {isOpen, onOpen, onClose} = useDisclosure();
@@ -12,26 +14,15 @@ export const Header: VFC = memo(() => {
             ユーザー管理アプリ
           </Heading>
         </Flex>
-
         <Flex align="center" fontSize="sm" flexGrow={2} display={{base: "none", md: "flex"}}>
           <Box pr={4}>
             <Link>ユーザー一覧</Link>
           </Box>{" "}
           <Link>設定</Link>
         </Flex>
-        <IconButton aria-label="メニューボタン" icon={<HamburgerIcon />} size="sm" variant="unstyled" display={{base: "block", md: "none"}} onClick={onOpen} />
+        <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay>
-          <DrawerContent>
-            <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">TOP</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
-            </DrawerBody>
-          </DrawerContent>
-        </DrawerOverlay>
-      </Drawer>
+      <MenuDrawer onClose={onClose} isOpen={isOpen} />
     </>
   );
 });
