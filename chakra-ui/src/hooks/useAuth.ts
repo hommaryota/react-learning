7b5fe1c54e0f3b6cf8a8a317d702a43f4a3fb7e7
hommaryota <kabuprivate@gmail.com>
2022-02-25 10:05:32 +0900

    loginUser Contxt保持

--- chakra-ui/src/hooks/useAuth.ts
+++ chakra-ui/src/hooks/useAuth.ts
@@ -4,31 +4,38 @@ import {useHistory} from "react-router-dom";
 
 import {User} from "../types/api/user";
 import {useMessage} from "./useMessage";
+import {useLoginUser} from "./useLoginUser";
 
 export const useAuth = () => {
   const history = useHistory();
   const {showMessage} = useMessage();
+  const {setLoginUser} = useLoginUser();
 
   const [loading, setLoading] = useState(false);
 
-  const login = useCallback((id: string) => {
-    setLoading(true);
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
 
-    axios
-      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        if (res.data) {
-          showMessage({title: "ログインしました", status: "success"});
-          history.push("/home");
-        } else {
-          showMessage({title: "ユーザーが見つかりません", status: "error"});
-        }
-      })
-      .catch(() => showMessage({title: "ログインできません", status: "error"}))
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            setLoginUser(res.data);
+            showMessage({title: "ログインしました", status: "success"});
+            history.push("/home");
+          } else {
+            showMessage({title: "ユーザーが見つかりません", status: "error"});
+            setLoading(false);
+          }
+        })
+        .catch(() => {
+          showMessage({title: "ログインできません", status: "error"});
+          setLoading(false);
+        });
+    },
+    [history, showMessage, setLoginUser]
+  );
 
   return {
     login,
